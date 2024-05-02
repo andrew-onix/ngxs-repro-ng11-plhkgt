c0a1555efd5ffca59fec299d636548cf5ef7a9c9
andrew-onix <166146676+andrew-onix@users.noreply.github.com>
2024-05-02 12:50:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngxs-repro-ng11-plhkgt
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/andrew-onix/ngxs-repro-ng11-plhkgt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Add, CountState } from './app.state';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Count is {{count$ | async}}</h1>
+    <button (click)="onClick()">Click Me</button>
+  `
+})
+export class AppComponent  {
+
+  @Select(CountState) count$: Observable<number>;
+
+  constructor(private store: Store) {}
+
+  onClick() {
+    this.store.dispatch(new Add());
+  }
+  
+}
